b26f4f855038b2d6532e076b29a216db8a5339f4
JonIHolton <142396435+JonIHolton@users.noreply.github.com>
2024-11-12 08:38:15 +0800

    Saga Orchestrator initial commit

---
+++ backend/sagaOrchestrator/src/test/java/com/csd/saga/SagaOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csd.saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
